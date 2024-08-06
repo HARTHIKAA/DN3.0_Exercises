18ea847e4bf400af6aacc8e01d75a7617d3f0ef9
HARTHIKA S <132971692+HARTHIKAA@users.noreply.github.com>
2024-08-06 19:42:58 +0530

    Create BookService.java

---
+++ Week_2_Exercises/Spring_Core_Maven/6. Configuring Beans with Annotations/BookService.java
@@ -0,0 +1,11 @@
+package com.library.service;
+
+import org.springframework.stereotype.Service;
+import com.library.repository.BookRepository;
+
+@Service
+public class BookService {
+    private BookRepository bookRepository;
+
+    // Setter method for bookRepository
+}
