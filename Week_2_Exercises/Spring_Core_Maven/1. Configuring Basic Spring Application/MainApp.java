b4c115375188e962b1cebb3aba41ab2078189780
HARTHIKA S <132971692+HARTHIKAA@users.noreply.github.com>
2024-08-06 19:15:31 +0530

    Create MainApp.java

---
+++ Week_2_Exercises/Spring_Core_Maven/1. Configuring Basic Spring Application/MainApp.java
@@ -0,0 +1,12 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService bookService = context.getBean("bookService", BookService.class);
+
+        // Test the configuration by calling a method on bookService
+        // ...
+    }
+}
