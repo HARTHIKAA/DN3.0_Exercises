7e08539972b20fa0b49f9f2c05ab77f054b5c7df
HARTHIKA S <132971692+HARTHIKAA@users.noreply.github.com>
2024-08-16 10:35:36 +0530

    Delete Week_3_Exercises/Employee Management System - Defining Query Methods directory

--- Week_3_Exercises/Employee Management System - Defining Query Methods/JpaRepository.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    @Query("SELECT e FROM Employee e WHERE e.department.name = :departmentName")
-    List<Employee> getEmployeesByDepartmentName(@Param("departmentName") String departmentName);
-}
