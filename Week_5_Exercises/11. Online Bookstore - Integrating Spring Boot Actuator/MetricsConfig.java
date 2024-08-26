d40cb22dcfe3e240a9578f86c3d980b1f33fdd98
HARTHIKA S <132971692+HARTHIKAA@users.noreply.github.com>
2024-08-26 14:25:49 +0530

    Create MetricsConfig.java

---
+++ Week_5_Exercises/11. Online Bookstore - Integrating Spring Boot Actuator/MetricsConfig.java
@@ -0,0 +1,14 @@
+package com.example.bookstoreapi.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfig {
+
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", "BookstoreAPI");
+    }
+}
