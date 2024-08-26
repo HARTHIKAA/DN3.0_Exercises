adb543d96f6c042e9263a4e146e5742162c25ecc
HARTHIKA S <132971692+HARTHIKAA@users.noreply.github.com>
2024-08-26 12:53:26 +0530

    Create CustomerController.java

---
+++ Week_4_Exercises/4. Online Bookstore - Processing Request Body and Form Data/CustomerController.java
@@ -0,0 +1,23 @@
+package com.example.bookstoreapi.controller;
+
+import com.example.bookstoreapi.model.Customer;
+import com.example.bookstoreapi.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    // POST: Create a new customer with JSON request body
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+        Customer savedCustomer = customerRepository.save(customer);
+        return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
+    }
+}
